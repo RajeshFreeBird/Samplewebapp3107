cba555fce181c9b2c1f11da8b6ffe8b5bd4e0b35
RajeshFreebird <rajeshkanna_india@yahoo.co.in>
2021-07-31 15:43:08 +0530

    Add project files.

---
+++ Samplewebqpp3107/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Samplewebqpp3107.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
